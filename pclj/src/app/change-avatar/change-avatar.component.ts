35f5dab2186c22e192fb37977dff7876ae29f676
Gustavo <gustavo@kapibara.com.br>
2023-07-27 21:12:54 -0300

    Adicionando opção de download no avatar

--- pclj/src/app/change-avatar/change-avatar.component.ts
+++ pclj/src/app/change-avatar/change-avatar.component.ts
@@ -110,4 +110,44 @@ export class ChangeAvatarComponent implements OnInit {
     else return true;
   }
 
+  onDownloadPNG = () => {
+    let svgNode = document.getElementById('svgid');
+    let canvas = document.createElement("canvas");
+    canvas.width = 400;
+    canvas.height = 408;
+    let ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    let anyWindow = window as any;
+    let DOMURL = anyWindow.URL || anyWindow.webkitURL || window;
+    let data = svgNode.innerHTML;
+    let svg = new Blob([data], { type: 'image/svg+xml' });
+    let img = new Image(canvas.width, canvas.height);
+    let url = DOMURL.createObjectURL(svg);
+    img.onload = () => {
+      ctx.save();
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+      ctx.restore();
+      DOMURL.revokeObjectURL(url);
+
+      let a = document.createElement('a');
+      a.href = canvas.toDataURL('image/png');
+      a.download = 'avatar-PCLJ.png';
+      a.click();
+
+      // canvas.toBlob(imageBlob => {
+        // this._uploadService.uploadBlobFotoPerfil(imageBlob)
+        //   .then((fileName) => {
+        //     this.loading = false;
+        //     this.setFotoDePerfil(fileName);
+        //   })
+        //   .catch((error) => {
+        //     this.loading = false;
+        //     abp.message.error("Erro no upload");
+        //   });
+      // });
+    };
+    img.src = url;
+  };
+
 }
