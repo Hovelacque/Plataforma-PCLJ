8c4321e721dad31cc690bb85c4a0a871ef613d19
Gustavo <gustavo@kapibara.com.br>
2023-09-07 18:39:31 -0300

    Liberando CORS

--- php/config.php
+++ php/config.php
@@ -1,5 +1,10 @@
 <?php
 
+//Configura o CORS
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, X-Token-Auth, Authorization");
+
 // $configFile = __DIR__ . '\config.json';
 $configFile = __DIR__ . '\config.desk.json';
 
