6b0953f533d375c16c8922d7f258b0bf3d968aea
Gustavo <gustavo@kapibara.com.br>
2023-09-07 18:39:20 -0300

    Adicionando configuração do PC

--- php/config.php
+++ php/config.php
@@ -1,5 +1,7 @@
 <?php
 
-$configFile = __DIR__ . '\config.json';
+// $configFile = __DIR__ . '\config.json';
+$configFile = __DIR__ . '\config.desk.json';
+
 $jsonString = file_get_contents($configFile);
 $_CONFIG = json_decode($jsonString, JSON_PRETTY_PRINT);
