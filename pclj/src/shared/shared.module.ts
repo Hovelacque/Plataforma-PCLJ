cff75e89c2a77b24c370094e995ec040d1593614
Gustavo <gustavo@kapibara.com.br>
2023-09-10 14:11:20 -0300

    Retirando módulos da shared

--- pclj/src/shared/shared.module.ts
+++ pclj/src/shared/shared.module.ts
@@ -7,16 +7,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { CustomPaginatorComponent } from './components/custom-paginator/custom-paginator.component';
 import { AppSessionService } from './session/app-session.service';
 import { AppRouteGuard } from './auth/auth-route-guard';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     HttpClientModule,
-    BrowserModule,
-    BrowserAnimationsModule,
 
     MatPaginatorModule,
     MatSnackBarModule
