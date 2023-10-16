41d24aa5afc8bf6506293ba51df4a611e02a219c
Gustavo <gustavo@kapibara.com.br>
2023-10-16 08:26:18 -0300

    Update insert-trabalhos.sql

--- sql/insert-trabalhos.sql
+++ sql/insert-trabalhos.sql
@@ -32,3 +32,107 @@ INSERT INTO `trabalho_aluno` (`trabalhoId`, `usuarioId`) VALUES
 (1, 6),
 (1, 7),
 (1, 8)
+
+-- Trabalho anime_multiplicacao
+INSERT INTO `trabalho_aluno` (`trabalhoId`, `usuarioId`) VALUES
+(2, 10),
+(2, 13),
+(2, 15),
+(2, 16),
+(2, 17),
+(2, 18),
+(2, 19),
+(2, 22),
+(2, 24),
+(2, 27),
+(2, 28),
+(2, 29),
+(2, 3),
+(2, 4),
+(2, 6),
+(2, 7),
+(2, 9)
+
+-- Trabalho jogo_quebra_blocos
+INSERT INTO `trabalho_aluno` (`trabalhoId`, `usuarioId`) VALUES
+(3, 10),
+(3, 11),
+(3, 13),
+(3, 14),
+(3, 15),
+(3, 16),
+(3, 17),
+(3, 18),
+(3, 19),
+(3, 20),
+(3, 22),
+(3, 23),
+(3, 24),
+(3, 27),
+(3, 28),
+(3, 29),
+(3, 4),
+(3, 5),
+(3, 6),
+(3, 7),
+(3, 9)
+
+-- Trabalho jogo_proprio
+INSERT INTO `trabalho_aluno` (`trabalhoId`, `usuarioId`) VALUES
+(4, 10),
+(4, 11),
+(4, 13),
+(4, 15),
+(4, 16),
+(4, 17),
+(4, 20),
+(4, 22),
+(4, 23),
+(4, 24),
+(4, 27),
+(4, 29),
+(4, 4),
+(4, 7),
+(4, 8),
+(4, 9),
+(4, 14)
+
+-- Trabalho anime_pclj
+INSERT INTO `trabalho_aluno` (`trabalhoId`, `usuarioId`) VALUES
+(5, 10),
+(5, 11),
+(5, 13),
+(5, 14),
+(5, 16),
+(5, 18),
+(5, 19),
+(5, 23),
+(5, 24),
+(5, 25),
+(5, 27),
+(5, 28),
+(5, 4),
+(5, 6),
+(5, 7),
+(5, 8),
+(5, 9)
+
+-- Trabalho jogo_digitacao
+INSERT INTO `trabalho_aluno` (`trabalhoId`, `usuarioId`) VALUES
+(6, 10),
+(6, 13),
+(6, 14),
+(6, 16),
+(6, 17),
+(6, 18),
+(6, 19),
+(6, 22),
+(6, 23),
+(6, 24),
+(6, 27),
+(6, 28),
+(6, 4),
+(6, 5),
+(6, 7),
+(6, 8),
+(6, 9)
